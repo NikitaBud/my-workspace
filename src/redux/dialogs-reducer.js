e20c54c95cac7357b9fd2980333b9ba750d2c871
NikitaBud <38902656+NikitaBud@users.noreply.github.com>
2019-10-09 13:48:55 +0200

    add reducer

---
+++ src/redux/dialogs-reducer.js
@@ -0,0 +1,34 @@
+const SEND_MESSAGE = 'SEND-MESSAGE';
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
+
+const dialogsReducer = (state, action) => {
+    switch (action.type) {
+        case SEND_MESSAGE:
+            let newMessage = {
+                id: 4,
+                message: state.newMessageText
+            };
+            state.messages.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.newMessage;
+            return state;
+        default:
+            return state;
+    }
+}
+
+export const sendMessageActionCreator = () => {
+    return {
+        type: SEND_MESSAGE
+    }
+}
+export const updateNewMessageTextActionCreator = (text) => {
+    return {
+        type: UPDATE_NEW_MESSAGE_TEXT,
+        newMessage: text
+    }
+}
+
+export default dialogsReducer;
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+
 let store = {
     _state: {
         profilePage: {
@@ -5,14 +8,15 @@ let store = {
                 {id: 1, message: 'Hi', likesCount: 12},
                 {id: 2, message: 'How are your?', likesCount: 11},
                 {id: 3, message: 'Yo-yo', likesCount: 22}
-            ]
+            ],
+            newPostText: ''
         },
         dialogsPage: {
             dialogs: [
                 {id: 1, name: 'Maria', img: '/princess-leia.png'},
                 {id: 2, name: 'Roman', img: '/alien-2.png'},
                 {id: 3, name: 'Danila', img: '/alien-4.png'},
-                {id: 4, name: 'Paul', img: '/atronaut.png'},
+                {id: 4, name: 'Paul', img: '/astronaut.png'},
                 {id: 5, name: 'Ivan', img: '/rick.png'},
                 {id: 6, name: 'Jura', img: '/darth-vader.png'}
             ],
@@ -20,31 +24,26 @@ let store = {
                 {id: 1, message: 'Hi'},
                 {id: 2, message: 'How are your?'},
                 {id: 3, message: 'Yo-yo'}
-            ]
+            ],
+            newMessageText: ''
         }
-    },
-    getState() {
-        return this._state;
     },
     _callSubscriber() {
         console.log('State changed');
     },
-    addPost() {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        };
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
-    },
-    updateNewPostText(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
+
+    getState() {
+        return this._state;
     },
     subscribe(observer) {
         this._callSubscriber = observer;
+    },
+
+    dispatch(action) {
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+
+        this._callSubscriber(this._state);
     }
 }
 
