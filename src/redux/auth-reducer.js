d03f79aaa58ad8f7332c55a7955273ef5e3c7ce7
Nikita Budiakov <budyakov91@gmail.com>
2020-02-04 15:25:02 +0100

    80-initialize, refactoring

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,4 +1,5 @@
 import {authAPI} from "../api/api";
+import {stopSubmit} from "redux-form";
 
 const SET_USER_DATA = 'SET_USER_DATA';
 
@@ -22,9 +23,12 @@ const authReducer = (state = initialState, action) => {
     }
 };
 
-export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USER_DATA, payload: {userId, email, login, isAuth}});
+export const setAuthUserData = (userId, email, login, isAuth) => ({
+    type: SET_USER_DATA, payload: {userId, email, login, isAuth}
+});
+
 export const getAuthUserData = () => (dispatch) => {
-    authAPI.me()
+    return authAPI.me()
         .then(response => {
             if (response.data.resultCode === 0) {
                 let {id, email, login} = response.data.data;
@@ -32,14 +36,19 @@ export const getAuthUserData = () => (dispatch) => {
             }
         });
 }
+
 export const login = (email, password, rememberMe) => (dispatch) => {
     authAPI.login(email, password, rememberMe)
         .then(response => {
             if (response.data.resultCode === 0) {
                 dispatch(getAuthUserData())
+            } else {
+                let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Common error';
+                dispatch(stopSubmit('login', {_error: message}));
             }
         });
 }
+
 export const logout = () => (dispatch) => {
     authAPI.logout()
         .then(response => {
